215a223c00b45bbaeab50672e60575b98809d7d4
Пузыно Эдуард <draconus72@gmail.com>
2021-09-13 12:36:20 +0300

    feature/BB-5: delete components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/index";
-import Api from "./components/atom/api/component";
+import Api from "./components/atom/Api";
 
 ReactDOM.render(
   <React.StrictMode>
