1ac690333237a3752f9533a5dcfedbd3fdca355d
Пузыно Эдуард <draconus72@gmail.com>
2021-09-24 11:31:36 +0300

    feature/BB-7-1: remake trouble

---
+++ src/components/organism/cardList/component.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PersonCard from "../../molecule/PersonCard";
+import Loader from "../../atom/Loader";
+import Error from "../../atom/Error";
+import style from "./style.module.scss";
+
+const CardList = ({ characters, isLoading, errorValue }) => {
+  const listItem = !errorValue ? characters.map((character) => (
+    <PersonCard
+      key={character.char_id}
+      name={character.name}
+      birthday={character.birthday}
+      img={character.img}
+      status={character.status}
+    />
+  )) : ('');
+
+  return (
+    <div>
+      {errorValue ? (
+        <Error textError={"Ошибка загрузки данных: " + errorValue} />
+      ) : !isLoading ? (
+        <div className={style.wrapper}>{listItem}</div>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+};
+
+export default CardList;
---
+++ src/components/organism/cardList/container.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import CardList from "./component";
+import { loadPersonList } from "../../../store/actions/importData";
+
+const PersonListContainer = () => {
+  const dispatch = useDispatch();
+  const characters = useSelector((state) => state.personListReducer.characters);
+  const isLoading = useSelector((state) => state.personListReducer.isLoad);
+  const errorValue = useSelector((state) => state.personListReducer.isError);
+
+  useEffect(() => {
+    dispatch(loadPersonList());
+  }, [dispatch]);
+
+  return <CardList characters={characters} isLoading={isLoading} errorValue={errorValue} />;
+};
+
+export const container = PersonListContainer;
