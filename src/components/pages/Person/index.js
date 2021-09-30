eb8bb47861a6d4037746aabd18e9fae5f8c005e6
Пузыно Эдуард <draconus72@gmail.com>
2021-09-30 13:05:07 +0300

    feature/BB-9: add to Person 'Назад в Каталог'

--- src/components/pages/Person/index.js
+++ src/components/pages/Person/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PersonSheet from "../../organism/PersonSheet";
 import style from "./style.module.scss";
 
 const Person = () => {
   return (
     <div className={style.main}>
+      <Link to={"/catalog"} className={style.backToCatalog} >
+        <div className={style.leftArrow} />
+        <p>Назад в Каталог</p>
+      </Link>
       <PersonSheet />
     </div>
   );
