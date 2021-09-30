061bf2bcd63d9fce6cdf3e8e2587cdc2b97b6f5c
Пузыно Эдуард <draconus72@gmail.com>
2021-09-30 11:13:09 +0300

    feature/BB-9: small correcting

--- src/components/organism/Header/index.js
+++ src/components/organism/Header/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import style from "./style.module.scss";
-import logo from "../../../assets/img/logo.svg";
+import logo from "../../../assets/img/logo.png";
 
 const Header = () => {
-  const catalogStyle = (useLocation().pathname === "/catalog") 
-       ? ( style.catalog + ' ' + style.forCardList )
-       : ( style.catalog );
+  const catalogStyle =
+    useLocation().pathname === "/catalog"
+      ? style.catalog + " " + style.forCardList
+      : style.catalog;
   return (
     <div className={style.main}>
-      <img className={style.image} src={logo} alt="Breaking Bad" />
-      <Link to={"/catalog"} className={ catalogStyle }>
+      <Link className={style.image} to={"/"}>
+        <img src={logo} alt="Breaking Bad" />
+      </Link>
+      <Link to={"/catalog"} className={catalogStyle}>
         <span className={style.threeLine}>
           <i></i>
           <i></i>
--- src/components/pages/Person/index.js
+++ src/components/pages/Person/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PersonSheet from "../../organism/PersonSheet";
+import style from "./style.module.scss";
 
 const Person = () => {
   return (
-    <div>
+    <div className={style.main}>
       <PersonSheet />
     </div>
   );
