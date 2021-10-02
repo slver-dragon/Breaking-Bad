431c7f51b67077d734accbb0d2ed2b0f7a21c9e7
Пузыно Эдуард <draconus72@gmail.com>
2021-10-02 15:57:49 +0300

    feature/BB-9: work on error adaptive

--- src/components/pages/Person/index.js
+++ src/components/pages/Person/index.js
@@ -2,20 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../../atom/Button";
 import PersonSheet from "../../organism/PersonSheet";
-import leftArrow from "../../../assets/img/leftArrow.png"
+import leftArrow from "../../../assets/img/leftArrow.png";
 import style from "./style.module.scss";
 
 const Person = () => {
   return (
     <div className={style.main}>
-      <Link to={"/catalog"} className={style.backToCatalog} >
-      <Button
-          img={leftArrow}
-          alt={"Назад в Каталог"}
-          text={"Назад в Каталог"}
-        />
-      </Link>
-      <PersonSheet />
+      <div className={style.container}>
+        <Link to={"/catalog"} className={style.backToCatalog}>
+          <Button img={leftArrow} text={"Назад в Каталог"} />
+        </Link>
+        <PersonSheet />
+      </div>
     </div>
   );
 };
