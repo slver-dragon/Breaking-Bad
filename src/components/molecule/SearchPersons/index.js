70ec48d36cd9b7abf095f57ad03667904fe4c85c
Пузыно Эдуард <draconus72@gmail.com>
2021-11-24 15:26:40 +0300

    feature/BB-16: add Enter event

--- src/components/molecule/SearchPersons/index.js
+++ src/components/molecule/SearchPersons/index.js
@@ -10,12 +10,15 @@ const SearchPersons = ({ search }) => {
     search('');
     request.current.value = '';
   }
+  const pressEnter = (event) => {
+    if (event.keyCode === 13) search(request.current.value)
+  }
   return (
     <label className={style.search}>
       <span onClick={() => discardSearch()}>
         <Button img={cross} />
       </span>
-      <input type="text" placeholder={"Поиск"} ref={request} />
+      <input type="text" placeholder={"Поиск"} ref={request} onKeyDown={(event) => {pressEnter(event)}} />
       <span onClick={() => search(request.current.value)}>
         <Button img={lens} text={"Найти"} />
       </span>
