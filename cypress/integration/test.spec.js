76a04a6294e4af91eba0eb5bde77092f9a17e5f4
Пузыно Эдуард <draconus72@gmail.com>
2021-12-13 20:05:49 +0300

    feature/BB-19: e2e test make

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,20 +1,33 @@
 describe("Breaking Bad app e2e test", () => {
-    it("New task enter", () => {
-      cy.visit("localhost:3000/Home");
-      cy.get('Button').click();
-    });
-  
-    // it("Change task status", () => {
-    //   cy.get('div').contains('NewTask').click();
-    // });
-  
-    // it("Task sorting", () => {
-    //   cy.get('span').contains('Complete').click();
-    //   cy.get('span').contains('Active').click();
-    //   cy.get('span').contains('All').click();
-    // });
-  
-    // it("New task delete", () => {
-    //   cy.get('div').contains('NewTask').siblings('div').click();
-    // });
+  beforeEach(() => {
+    cy.visit("localhost:3000");
   });
+
+  it("Click serfing", () => {
+    cy.get(".style_catalog__2Tf0B").click();
+    cy.get('[href="/person/2"]').click();
+    cy.get(".style_backToCatalog__14kGk > .style_button__3ugsu").click();
+    cy.get('#PutListFormat > .style_button__3ugsu > img').click();
+    cy.get('input').type('Walter');
+    cy.get('#Search > .style_button__3ugsu').click();
+    cy.get('#Clear > .style_button__3ugsu > img').click();
+    cy.get('[href="/"] > .style_button__3ugsu > img').click();
+  });
+
+  it("Change count element & number page", () => {
+    cy.get(".style_catalog__2Tf0B").click();
+    cy.get(
+      ".style_elementPerPage__rEa7a > :nth-child(4) > .style_button__3ugsu > span"
+    ).click();
+    cy.get(
+      ".style_pages__1ArFM > :nth-child(3) > .style_button__3ugsu > span"
+    ).click();
+    cy.get(':nth-child(7) > .style_button__3ugsu > img').click();
+    cy.get('.style_pages__1ArFM > :nth-child(5) > .style_button__3ugsu > img').click();
+
+  });
+
+  // it("New task delete", () => {
+  //   cy.get('div').contains('NewTask').siblings('div').click();
+  // });
+});
