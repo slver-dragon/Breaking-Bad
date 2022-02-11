851344872876341308989729f301603b4c5bc04d
Пузыно Эдуард <draconus72@gmail.com>
2022-02-11 10:34:56 +0300

    bugfix/main: conflict resolve

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/organism/Header";
+import Footer from "./components/organism/Footer";
+import Home from "./components/pages/Home";
+import Catalog from "./components/pages/Catalog";
+import Person from "./components/pages/Person";
+import style from "./style.module.scss";
+
+function App() {
+  const isHomePage = useSelector((state) => state.services.isHomePage);
+  const wrapperStyle = isHomePage ? style.wrapperHome : style.wrapper;
+  return (
+    <div className={style.body}>
+      <div className={wrapperStyle}>
+        <BrowserRouter>
+          <div className={style.header}>
+            <Header />
+          </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/catalog" component={Catalog} />
+            <Route path="/person/:id" component={Person} />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
